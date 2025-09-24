b9efeb376929e8251f73480981b981220b367632
Jazmin Rios Rojas <jazminriosrojas80@gmail.com>
2025-09-23 21:33:17 -0600

    Proyecto 1

--- Sumatoria.py
+++ Sumatoria.py
@@ -1,18 +1,11 @@
-'''
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-'''
 v = []
-for i in range (5):
-    valor = int(input("ingrese el valor: "))
+for i in range (6):
+    valor = int(input(f"asigna un valor{i+1}: "))
     v.append(valor)
 pos = 0
-ac = 0 
+ac = 0
 while pos < len(v):
     ac = ac + v[pos]
-    pos = pos + 1 
-print ("la suma es: ",ac)
+    pos += 1
+print(ac)
+print(v)
